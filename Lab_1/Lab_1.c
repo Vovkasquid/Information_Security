f15e0f96dd81ffb4f129768360f2de08a18e67ab
Vladimir Blinov <vovka_squid@mail.ru>
2015-11-23 21:09:02 +0300

    Lab_1: may me done
    Signed-off-by: Vladimir Blinov <vovka_squid@mail.ru>

---
+++ Lab_1/Lab_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//Library for uint64_t
+#include <inttypes.h>
+
+int main() {
+	uint64_t m, a, b;
+	//Just for test
+	printf("uint64_t size: %lu\n",sizeof(a));
+	
+	scanf("%lu", &m);
+	scanf("%lu", &a);
+	scanf("%lu", &b);
+	//Input validation
+	printf("M: %lu\nA: %lu\nB %lu\n", m, a, b);
+	//Если условие выполняется, то можно спокойно вычитать
+	if (a < (m - b)) {
+		printf("Result of adding: %lu\n", a+b);
+	} else { //Иначе легкая математика
+		printf("Result of adding: %lu\n", m - ((m - a) + (m - b)));
+	}
+	return 0;
+}
